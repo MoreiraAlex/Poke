aabf31da21e2f00750111da9ad4c4c11255d451d
Alex Moreira <amoreira@allianceconsultoria.com.br>
2026-05-13 02:57:45 -0300

    feat: add PWA support with icons and update dependencies
    - Added vite-plugin-pwa to enable Progressive Web App features.
    - Included icons for the PWA: icon-192.png and icon-512.png.
    - Updated package.json to include vite-plugin-pwa as a dependency.
    - Configured Vite to use PWA with auto-update and caching settings.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
-/**
- * @type {import('vite').UserConfig}
- */
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default {
   base: '/Poke/',
   build: {
     sourcemap: true,
   },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+
+      manifest: {
+        name: 'Pokemon Game',
+        short_name: 'Pokemon',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#000000',
+
+        icons: [
+          {
+            src: '/Poke/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/Poke/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 }
